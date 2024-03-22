48cdc65b829384933a0b60afb94cf4097b0bff53
mohamed-elmashad <mohamedapple@hotmail.com>
2024-03-22 04:39:44 -0700

    Add convergence to end simulating

--- src/agent.py
+++ src/agent.py
@@ -3,6 +3,10 @@
 import numpy as np
 import math
 import random
+import csv
+import itertools as it
+import os
+import time
 
 
 
@@ -21,6 +25,7 @@ class Agent:
         self.discount_factor = 0.99
         self.min_explore_rate = 0.001
         self.min_learning_rate = 0.1
+        self.decay_type = "log"
         self.decay_factor = np.prod(self.env.maze_size, dtype=float) / 10.0
 
     def select_action(self, state, explore_rate):
@@ -29,23 +34,26 @@ class Agent:
         else:
             return int(np.argmax(self.q_table[state]))
         
-    def get_explore_rate(self, t, decay_type="log"):
-        if decay_type == "log":
+    def set_decay_type(self, decay_type):
+        self.decay_type = decay_type
+        
+    def get_explore_rate(self, t):
+        if self.decay_type == "log":
             return max(self.min_explore_rate, min(0.8, 1.0 - math.log10((t + 1) / self.decay_factor)))
-        elif decay_type == "linear":
+        elif self.decay_type == "linear":
             return max(self.min_explore_rate, min(1, 1.0 - (t + 1) / self.decay_factor))
-        elif decay_type == "exponential":
+        elif self.decay_type == "exponential":
             return max(self.min_explore_rate, min(0.8, 0.99 ** (t / self.decay_factor)))
         else:
             raise ValueError("Invalid decay type")
 
 
-    def get_learning_rate(self, t, decay_type="log"):
-        if decay_type == "log":
+    def get_learning_rate(self, t):
+        if self.decay_type == "log":
             return max(self.min_learning_rate, min(0.8, 1.0 - math.log10((t + 1) / self.decay_factor)))
-        elif decay_type == "linear":
+        elif self.decay_type == "linear":
             return max(self.min_learning_rate, min(1, 1.0 - (t + 1) / self.decay_factor))
-        elif decay_type == "exponential":
+        elif self.decay_type == "exponential":
             return max(self.min_learning_rate, min(0.8, 0.99 ** (t / self.decay_factor)))
         else:
             raise ValueError("Invalid decay type")
@@ -94,12 +102,56 @@ class Agent:
             print("\n")
 
 
+    def log(self, episode, total_reward, num_steps, title):
+        csv_file = title + '_log.csv'
+        with open(csv_file, 'a', newline='') as csvfile:
+            fieldnames = ['Episode', 'Total Reward', 'Num Steps']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            if csvfile.tell() == 0:
+                writer.writeheader()
+
+            writer.writerow({'Episode': episode, 'Total Reward': total_reward, 'Num Steps': num_steps})
+
+    def auto_log(self, num_times=10):
+        learning_rates = ["log", "linear", "exponential"]
+        discount_factors = [0.99, 0.995, 0.95, 0.9]
+
+        win_rewards = [1, 10, 100, 1000]
+        step_rewards = [-0.1, -0.01, -0.001, -0.0001]
+        wall_rewards = [-100, -1000, -10000]
+
+        decay_factors = [10, 100, 1000, 10000]
+
+
+        logs_folder = "data"
+        for run_id in range(num_times):
+            run_folder = os.path.join(logs_folder, f"run_{run_id}")
+            os.makedirs(run_folder, exist_ok=True)
+            os.chdir(run_folder)
+            time.sleep(60)
+        
+            # lr = learning rate, df = discount factor, wr = win reward, sr = step reward, wr = wall reward, dy = decay factor
+            for lr, df, wr, sr, wr, dy in it.product(learning_rates, discount_factors, win_rewards, step_rewards, wall_rewards, decay_factors):
+                log_name = f"maze_{self.maze_size}_lr_{lr}_df_{df}_wr_{wr}_sr_{sr}_wr_{wr}_dy_{dy}"
+                self.discount_factor = df
+                self.set_decay_type(lr)
+                self.env.set_reward("win", wr)
+                self.env.set_reward("step", sr)
+                self.env.set_reward("wall", wr)                
+                
+                self.decay_factor = dy
+                self.simulate()
+                self.log(log_name)
+
     def simulate(self):
         num_streaks = 0
         learning_rate = self.get_learning_rate(0)
         explore_rate = self.get_explore_rate(0)
         self.env.reset()
-        
+
+        prev_total_reward = [0] * 30
+
         for episode in range(self.num_episodes):
             self.env.reset()
 
@@ -122,8 +174,10 @@ class Agent:
                 state_0 = state
 
                 if done:
+                    if self.logging_enabled:
+                        self.log(episode, total_reward, t, "q_learning")
                     print(f"Episode {episode} finished after {t} time steps with total reward = {total_reward} (streak {num_streaks}).")
-                    if t < np.prod(self.env.maze_size) and total_reward > 2:
+                    if t < np.prod(self.env.maze_size):
                         num_streaks += 1
                     else:
                         num_streaks = 0
@@ -132,9 +186,12 @@ class Agent:
                     print(f"Episode {episode} timed out at {t} time steps with total reward = {total_reward} (streak {num_streaks}).")
                     break
             
-            if num_streaks > 100:
-                print(f"Solved after {episode} episodes.")
+            if abs(total_reward - np.mean(prev_total_reward)) < 0.1:
+                print(f'Coneverged at episode {episode}')
                 break
+            else:
+                prev_total_reward.pop(0)
+                prev_total_reward.append(total_reward)
 
             explore_rate = self.get_explore_rate(episode)
             learning_rate = self.get_learning_rate(episode)
